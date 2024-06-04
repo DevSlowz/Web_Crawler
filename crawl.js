8ab9292b02fa64e766dd05b706fc30a1ec185377
DevSlowz <msvieira021@gmail.com>
2024-06-04 07:38:29 -0400

    Implement normalizeURL with the ability to remove leading and trailing characters and return normalized url

--- crawl.js
+++ crawl.js
@@ -1,5 +1,24 @@
-function normalizeURL(){
+function normalizeURL(url){
+// URL object
+// hostname 
+// pathname
 
+    // If a protocol is not attatch prpend protocol
+    if (!/^https?:\/\//i.test(url)) {
+        url = 'http://' + url;
+    }
+    // Create URL Object 
+    const normalizedURL = new URL(url.toLowerCase())
+
+    // Extract hostname from URL object
+    const hostname = normalizedURL.hostname
+    // Extract path from URL object and remove trailing /
+    let path = normalizedURL.pathname
+    if (path.endsWith('/')){
+        path = path.slice(0, -1);
+    }
+
+    return `${hostname}${path}`
 }
 
 
