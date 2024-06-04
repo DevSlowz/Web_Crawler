6485a4a2a52c97fd16ec9bb9f5900ff203345554
DevSlowz <msvieira021@gmail.com>
2024-06-04 07:08:56 -0400

    Define test for normalizeURL

--- crawl.test.js
+++ crawl.test.js
@@ -1,2 +1,27 @@
 import { test, expect } from "@jest/globals";
 import { normalizeURL } from "./crawl.js";
+
+
+test('Capital letters', () => {
+    expect(normalizeURL('https://Blog.Boot.Dev/Path/')).toBe('blog.boot.dev/path')
+})
+
+test('HTTPS with tailing slash', () => {
+    expect(normalizeURL('https://blog.boot.dev/path/')).toBe('blog.boot.dev/path')
+})
+
+test('HTTPS no tailing slash', () => {
+    expect(normalizeURL('https://blog.boot.dev/path')).toBe('blog.boot.dev/path')
+})
+
+test('HTTP with tailing slash', () => {
+    expect(normalizeURL('http://blog.boot.dev/path/')).toBe('blog.boot.dev/path')
+})
+
+test('HTTP no tailing slash', () => {
+    expect(normalizeURL('http://blog.boot.dev/path/')).toBe('blog.boot.dev/path')
+})
+
+test('No protocol', () => {
+    expect(normalizeURL('blog.boot.dev/path/')).toBe('blog.boot.dev/path')
+})
