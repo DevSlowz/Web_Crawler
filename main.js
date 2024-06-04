d9b270359c8cc9fa444374526db0d2c974550bf5
DevSlowz <msvieira021@gmail.com>
2024-06-04 09:57:55 -0400

    Implement main to accept command line argument from user to crawl website

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('hello world')
+function main() {
+    if (process.argv.length < 3) {
+      console.log('no website provided')
+      return
+    }
+    if (process.argv.length > 3) {
+      console.log('too many arguments provided')
+      return
+    }
+    const baseURL = process.argv[2]
+    // console.log(process.argv[0])
+    // console.log(process.argv[1])
+  
+    console.log(`starting crawl of: ${baseURL}...`)
+  }
+  
+  main()
+  
