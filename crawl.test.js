5b00c2c0b71d9e99827ff02abebac7405a1f719b
DevSlowz <msvieira021@gmail.com>
2024-06-04 08:27:35 -0400

    Add inital test for url conversion and verifying anchor tags

--- crawl.test.js
+++ crawl.test.js
@@ -1,7 +1,7 @@
 import { test, expect } from "@jest/globals";
-import { normalizeURL } from "./crawl.js";
+import { normalizeURL, getURLsFromHTML } from "./crawl.js";
 
-
+// Test for normalizeURL
 test('Capital letters', () => {
     expect(normalizeURL('https://Blog.Boot.Dev/Path/')).toBe('blog.boot.dev/path')
 })
@@ -24,4 +24,47 @@ test('HTTP no tailing slash', () => {
 
 test('No protocol', () => {
     expect(normalizeURL('blog.boot.dev/path/')).toBe('blog.boot.dev/path')
+})
+
+
+// getURLsFromHTML tests
+test('relative url converted to absolute url', () => {
+    const html = `<!DOCTYPE html>
+    <html>
+    <head>
+        <title>Sample Page</title>
+    </head>
+    <body>
+        <a href="/page1.html">Page 1</a>
+        <br>
+        <a href="/page2.html">Page 2</a>
+    </body>
+    </html>`
+    const url = 'https://blog.boot.dev'
+    const actual = getURLsFromHTML(html,url)
+    const expected = null
+    actual.forEach(link => {
+        expect(link).toBe(`${url}/${link}`)
+    });
+    
+})
+
+
+test('verify all anchor are found in the body', () => {
+    const html = `<!DOCTYPE html>
+    <html>
+    <head>
+        <title>Sample Page</title>
+    </head>
+    <body>
+        <a href="/page1.html">Page 1</a>
+        <br>
+        <a href="/page2.html">Page 2</a>
+    </body>
+    </html>`
+    const url = 'https://www.example.com'
+    const actual = getURLsFromHTML(html,url)
+    const expected = ['https://www.example.com/page1.html', 'https://www.example.com/page2.html']
+    expect(actual).toBe(expected)
+    
 })
