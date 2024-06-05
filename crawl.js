620bb4f414a8913a3d7085afdc5c2c47c059f61d
DevSlowz <msvieira021@gmail.com>
2024-06-05 07:26:47 -0400

    Fix bugs in fetchAndParseHTML and crawlPage Fixed bug where currentURLHost was not comparing to basedURL host Fixed bug where pages count for each url was not being incremented Implement promise all to allow for recursive calls to be run in parallel Fix variable naming conventions in fetchAndParseHTML

--- crawl.js
+++ crawl.js
@@ -46,57 +46,70 @@ function getURLsFromHTML(htmlBody, baseURL) {
     return links
 }
 
+// async function crawlPage(baseURL, currentURL = baseURL, pages = {}) {
+
+//     let webpage
+//     // Attempt to fetch information from specified URL
+//     try {
+//         webpage = await fetch(currentURL)
+//     } catch (error) {
+//         throw new Error(`Got Network error: ${error.message}`)
+//     }
+    
+//     // If the response from the fetch is equivalent to an error(>400) displaay to console
+//     if (webpage.status >= 400) {
+//         let err = new Error(`Got HTTP error: ${webpage.status} ${webpage.statusText}`)
+//         console.log(err.message)
+//         return 
+//     }
+
+//     // If the response from the fetch is not a valid type display error to console
+//     if (!webpage.headers.get('Content-Type').includes('text/html')){
+//         let err = new Error(`Got non-HTML response: ${contentType}`)
+//         console.log(err.message)
+//         return 
+//     }
+
+//     // Collect, Store, and display web content
+//     const webpageContent = await webpage.text()
+    
+//     console.log(webpageContent)
+// }
+
 async function crawlPage(baseURL, currentURL = baseURL, pages = {}) {
-
-    let webpage
-    // Attempt to fetch information from specified URL
-    try {
-        webpage = await fetch(currentURL)
-    } catch (error) {
-        throw new Error(`Got Network error: ${error.message}`)
-    }
-    
-    // If the response from the fetch is equivalent to an error(>400) displaay to console
-    if (webpage.status >= 400) {
-        let err = new Error(`Got HTTP error: ${webpage.status} ${webpage.statusText}`)
-        console.log(err.message)
-        return 
-    }
-
-    // If the response from the fetch is not a valid type display error to console
-    if (!webpage.headers.get('Content-Type').includes('text/html')){
-        let err = new Error(`Got non-HTML response: ${contentType}`)
-        console.log(err.message)
-        return 
-    }
-
-    // Collect, Store, and display web content
-    const webpageContent = await webpage.text()
-    
-    console.log(webpageContent)
-}
-
-async function crawlPageR(baseURL, currentURL = baseURL, pages = {}) {
-    let count
-    let webpageContent
     let URLs
     let currentURLHost = new URL(currentURL)
     // Verify currentURL is on the same domain as the baseURL
-    if (!currentURLHost.host === baseURL) {
+    if (currentURLHost.host !== new URL(baseURL).host) {
         return pages
     }
 
     // Normalized URL
-    normalURL = normalizeURL(currentURL)
-    if (pages.includes(normalURL)) {
-        count++
+    let normalURL = normalizeURL(currentURL)
+    if (normalURL in pages) {
+        pages[normalURL]++
+        return pages
+    }
+    else{
+        pages[normalURL] = 1 
+    }
+
+    let webpageContent = ''
+    console.log(`crawling ${currentURL}`)
+    try {
+        webpageContent = await fetchAndParseHTML(currentURL)
+    } catch (error) {
+        console.log(`${err.message}`)
         return pages
     }
 
-    webpageContent = fetchAndParseHTML(currentURL)
+    // Get urls from HTML bodies from webpageContent
     URLs = getURLsFromHTML(webpageContent,currentURL)
+    // Crawl Each URL
+    await Promise.all(URLs.map(url => crawlPage(baseURL, url, pages)));
 
-
+    return pages
+    
 }
 
 async function fetchAndParseHTML(currentURL) {
@@ -109,18 +122,18 @@ async function fetchAndParseHTML(currentURL) {
     }
     
     // If the response from the fetch is equivalent to an error(>400) displaay to console
-    if (webpage.status >= 400) {
-        let err = new Error(`Got HTTP error: ${webpage.status} ${webpage.statusText}`)
-        console.log(err.message)
+    if (webpage.status > 399) {
+        console.log(`Got HTTP error: ${webpage.status} ${webpage.statusText}`)
         return 
     }
 
-    // If the response from the fetch is not a valid type display error to console
-    if (!webpage.headers.get('Content-Type').includes('text/html')){
-        let err = new Error(`Got non-HTML response: ${contentType}`)
-        console.log(err.message)
-        return 
+    const contentType = webpage.headers.get('content-type')
+    if (!contentType || !contentType.includes('text/html')) {
+        console.log(`Got non-HTML response: ${contentType}`)
+        return
     }
+
+    // console.log(await webpage.text())
 
     // Collect, Store, and display web content
     const webpageContent = await webpage.text()
